e48538ac7741b754420a927834460346f2c46ead
Rafał Woźniczko <kller000@gmail.com>
2015-06-22 10:13:47 +0200

    Super zmiany kurwo

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import cv2
 camera = cv2.VideoCapture(0)
 
 while True:
-	ret, frame = camera.read()
+	ret, frame = camera.read(False)
 	frame = cv2.flip(frame,1)
+	
+
 	cv2.imshow('image', frame)
-	cv2.waitKey(1)
+	key = cv2.waitKey(1)
+	if key == 27:
+		break
+
+
 cv2.destroyAllWindows()
