8ecfe1b91bea52db4e73f4be20066c29d73273a3
Rafał Woźniczko <kller000@gmail.com>
2015-06-22 10:18:33 +0200

    Super repo kurwo

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import cv2
 camera = cv2.VideoCapture(0)
 
 while True:
-	ret, frame = camera.read(False)
+	ret, frame = camera.read()
 	frame = cv2.flip(frame,1)
 	
-
-	cv2.imshow('image', frame)
 	key = cv2.waitKey(1)
 	if key == 27:
 		break
+	frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	cv2.imshow('image', frame)
+	
 
 
 cv2.destroyAllWindows()
